25e18656605b81fbe86423060c215aedc526bf1d
Gabriel <longattigabriel@gmail.com>
2020-09-06 18:06:17 -0300

    feat: advanced refs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback } from "react";
+
+import Input from "./components/Input";
+import Modal, { ModalHandles } from "./components/Modal";
+
+import "./App.css";
 
 function App() {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const acceptTermsRef = useRef({ value: false });
+  const modalRef = useRef<ModalHandles>(null);
+
+  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    nameInputRef.current?.focus();
+    console.log(acceptTermsRef.current.value);
+  }, []);
+
+  const handleAcceptTerms = useCallback(() => {
+    acceptTermsRef.current.value = !acceptTermsRef.current.value;
+  }, []);
+
+  const handleOpenModal = useCallback(() => {
+    modalRef.current?.openModal();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <Input name="name" label="Nome completo" ref={nameInputRef} />
+
+        <button type="button" onClick={handleAcceptTerms}>
+          Aceitar termos
+        </button>
+
+        <button type="submit">Realizar Foco</button>
+      </form>
+
+      <button onClick={handleOpenModal}>Abrir Modal</button>
+
+      <Modal ref={modalRef} />
     </div>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,20 @@
+import React, { forwardRef } from "react";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+const Input: React.RefForwardingComponent<HTMLInputElement, InputProps> = (
+  { name, label, ...rest },
+  ref
+) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>{label}</label>
+      <input type="text" ref={ref} {...rest} />
+    </div>
+  );
+};
+
+export default forwardRef(Input);
